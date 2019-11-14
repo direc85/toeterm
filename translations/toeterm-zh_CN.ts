7826d24aeaa438a6a4912acea55843b608e42deb
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-11-14 13:41:27 +0800

    Create toeterm-zh_CN.ts

---
+++ translations/toeterm-zh_CN.ts
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="36"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="76"/>
+        <source>Terminal size</source>
+        <translation>Terminal 大小</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="98"/>
+        <source>Charset</source>
+        <translation>字符集</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="116"/>
+        <source>Based on ThumbTerm by Olli Vanhoja, which is fork of FingerTerm by Heikki Holstila</source>
+        <translation>基于 OlliVanhoja 开发的 ThumbTerm( 即Heukki Holstila FingerTerm 的分支软件)。</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="128"/>
+        <source>Config files for adjusting settings are at:</source>
+        <translation>用以调整设置的配置文件位于:</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/MainPage.qml" line="302"/>
+        <source>An error occurred</source>
+        <translation>发生错误</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsMenu</name>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="66"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="70"/>
+        <source>New window</source>
+        <translation>新窗口</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="92"/>
+        <location filename="../qml/SettingsMenu.qml" line="168"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="102"/>
+        <source>Paste</source>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="115"/>
+        <source>Actions</source>
+        <translation>操作</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="151"/>
+        <source>URL grabber</source>
+        <translation>URL grabber</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="164"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="188"/>
+        <source>Keyboard layout</source>
+        <translation>键盘布局</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="221"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="226"/>
+        <source>Font size</source>
+        <translation>字体大小</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="245"/>
+        <source>Drag mode</source>
+        <translation>拖拽模式</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="251"/>
+        <source>Gestures</source>
+        <translation>手势</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="251"/>
+        <source>Scroll</source>
+        <translation>页面滚动</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="251"/>
+        <source>Select</source>
+        <translation>选择</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="261"/>
+        <source>Keyboard behavior</source>
+        <translation>键盘行为</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="267"/>
+        <source>Off</source>
+        <translation>下移</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="267"/>
+        <source>Fade</source>
+        <translation>淡出</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="267"/>
+        <source>Move</source>
+        <translation>移动</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="277"/>
+        <source>Keyboard delay</source>
+        <translation>键盘延迟</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="281"/>
+        <source>ms</source>
+        <translation>毫米</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="281"/>
+        <source>No delay</source>
+        <translation>无延迟</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="297"/>
+        <source>Keyboard feedback</source>
+        <translation>键盘反馈</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="303"/>
+        <source>Charset</source>
+        <translation>字符集</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="312"/>
+        <source>Custom</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="332"/>
+        <source>Apply with Enter key</source>
+        <translation>按下 Enter 键以应用</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="333"/>
+        <source>Custom charset</source>
+        <translation>自定义字符集</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="349"/>
+        <source>Transparent background</source>
+        <translation透明背景></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="361"/>
+        <source>Color scheme</source>
+        <translation>颜色设计</translation>
+    </message>
+</context>
+</TS>
